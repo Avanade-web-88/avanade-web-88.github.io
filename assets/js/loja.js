6a146f72e82eaf6427327c26d8fb323d569cb4cc
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2022-10-06 15:31:34 -0300

    Deploy

--- assets/js/loja.js
+++ assets/js/loja.js
@@ -1,28 +1,7 @@
 // const body = document.querySelector(".body");
-const button = document.querySelector(".btn");
-let i = 1
-button.addEventListener('click', async (e) => {
-    
-    const options = {
-        method: "GET",
-        mode: "cors",
-        cache: "default"
-
-    }
-    // for (let i = 1; i <= 10; ++i) {
-        const resultado = await fetch(`https://dummyjson.com/products/${i}`, options)
-        const json = await resultado.json();
-        i++
-        if(i==11){
-            i = 1
-        };
-        console.log(json);
-    // };
-});
-
-
-
-// async function produtos(e) {
+// const button = document.querySelector(".btn");
+// let i = 1;
+// button.addEventListener('click', async (e) => {
 
 //     const options = {
 //         method: "GET",
@@ -30,12 +9,35 @@ button.addEventListener('click', async (e) => {
 //         cache: "default"
 
 //     }
-//     for (let i = 1; i <= 10; ++i) {
-//         const resultado = await fetch(`https://dummyjson.com/products/${i}`, options)
-//         const json = await resultado.json();
-
-//         console.log(json);
+//     // for (let i = 1; i <= 10; ++i) {
+//     const resultado = await fetch(`https://dummyjson.com/products/${i}`, options)
+//     const json = await resultado.json();
+//     ++i;
+//     if (i === 11) {
+//         i = 1
 //     };
-// }
+//     document.querySelector("body").innerHTML = `<div> <h1> ${json.title} <h1/> </div>`
+//     console.log(json);
+//     // };
+// });
 
-// produtos();
+
+
+async function produtos(e) {
+
+    const options = {
+        method: "GET",
+        mode: "cors",
+        cache: "default"
+
+    }
+    for (let i = 1; i <= 10; ++i) {
+        const resultado = await fetch(`https://dummyjson.com/products/${i}`, options)
+        const json = await resultado.json();
+        document.querySelector('body').innerHTML = `<div> <h1> ${json.title} <h1/> <h2>${json.description}<h2/> </div>`
+        
+        console.log(json);
+    };
+}
+
+produtos();
